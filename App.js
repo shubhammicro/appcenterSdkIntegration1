de31106d06f4a0e58f9cc40fe38166c4a12ef8c5
Shubham Zinjad <shubham.zinjad@microtelecom.com>
2020-04-06 14:14:40 +0530

    11th update

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import codePush from 'react-native-code-push';
 
-const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_START };
-
 class App extends Component {
+  const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_START };
   render() {
     return (
       <View>
